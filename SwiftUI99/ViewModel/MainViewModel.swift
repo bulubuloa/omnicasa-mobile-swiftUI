21a5296fac897b9a5e5800a2c5674842be8e600f
hoangquach <quachhoangqb@gmail.com>
2023-08-08 09:38:34 +0700

    Remove oauth token when it expired

--- SwiftUI99/ViewModel/MainViewModel.swift
+++ SwiftUI99/ViewModel/MainViewModel.swift
@@ -13,12 +13,13 @@ class MainViewModel: BaseViewModel {
     @Published var tokenExpired: String = ""
     
     func checkTokenAlive() {
-        let result = authenticationService.oauthTokenExpired() {
+         authenticationService.oauthTokenExpired() {
             res in
             DispatchQueue.main.async {
                 switch res {
                     case .success(let expired):
                         self.tokenExpired = "Token expired, need to re-login = \(expired)"
+                        self.settingDataStore.saveSetting(keyName: SettingKeys.oauthToken.rawValue, keyValue: nil)
                     case .failure(let error):
                         self.tokenExpired = "Token still alive, continue to use \(error.localizedDescription)"
                 }
